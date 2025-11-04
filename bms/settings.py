e88fffc9ba85ecc97f9026d45a5f8b22ffc766fc
antor <antorzuck@gmail.com>
2025-11-04 22:03:19 +0600

    asfd

--- bms/settings.py
+++ bms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_-b$y&ai+o_m&jjye@(+j$_k1zf18=^&sb1=%r-n@=fuij@^2b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'bms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
